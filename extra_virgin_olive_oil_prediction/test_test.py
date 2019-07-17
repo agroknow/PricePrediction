424fdd3a0709450eb7330dca698612744f42cc27
MihalisP <mihalispapak@gmail.com>
2019-07-17 16:05:05 +0300

    int. commit for multiple cols at reshape

--- extra_virgin_olive_oil_prediction/test_test.py
+++ extra_virgin_olive_oil_prediction/test_test.py
@@ -40,6 +40,8 @@ def read_cleanse(train=True, dfevoo=None):
         dfevoo['month'] = pd.DatetimeIndex(dfevoo['date']).month
         dfevoo['day'] = pd.DatetimeIndex(dfevoo['date']).day
 
+        # dfevoo['year'] = pd.to_timedelta(dfevoo['priceStringDate'], unit='ns').dt.total_seconds().astype(int)
+
         # dfevoo = dfevoo.sample(frac=1)
 
         dataset = dfevoo[columns]
@@ -76,18 +78,16 @@ test_labels = test_dataset.pop(target)
 # print(train_labels)
 
 scaler = MinMaxScaler(feature_range=(0, 1))
-scaled_data = scaler.fit_transform(train_dataset)
+dataset.pop('price')
+scaler.fit(dataset)
+scaled_data = scaler.transform(train_dataset)
 
+print(train_dataset)
+print(scaled_data)
+print(scaled_data[0:10, 0])
+print(scaled_data[0:10])
+# quit(0)
 # scaled_data_train=scaler.fit_transform(train_dataset)
-
-
-# scaled_data_train =[]
-#
-# scaled_data_test =[]
-# for i in range(1, len(train_dataset)):
-#     scaled_data_train = scaled_data[i,0]
-# #
-#     scaled_data_test = scaled_data[i,0]
 
 # Convert Training Data to Right Shape
 lb = 10
@@ -95,27 +95,22 @@ normed_train_data = []
 normed_test_data = []
 # # execute a loop that starts from 61st record and stores all the previous 60 records to the x_train list. The 61st record is stored in the y_trainlabels list.
 for i in range(lb, len(train_dataset)):
-    normed_train_data.append(scaled_data[i - lb:i, 0])
-    normed_test_data.append(scaled_data[i, 0])
+    normed_train_data.append(scaled_data[i - lb:i])
 #
 normed_train_data = np.array(normed_train_data)
-normed_test_data = np.array(normed_test_data)
 
-print(normed_train_data)
-
-# scaled_data_train=np.array(scaled_data_train)
-# scaled_data_test=np.array(scaled_data_test)
-
-normed_train_data = np.reshape(normed_train_data, (normed_train_data.shape[0], normed_train_data.shape[1], 1))
+print(normed_train_data.shape)
+normed_train_data = np.reshape(normed_train_data, (normed_train_data.shape[0], normed_train_data.shape[1], 3))
 # normed_train_data = np.reshape(scaled_data_train, (scaled_data_train.shape[0], scaled_data_train.shape[1], 1))
 # normed_train_test = np.reshape(scaled_data_test, (scaled_data_test.shape[0], scaled_data_test.shape[1], 1))
 
+# quit(0)
 print(normed_train_data)
 
 
 def build_model():
     t_model = Sequential()
-    t_model.add(LSTM(units=50, return_sequences=True, input_shape=(normed_train_data.shape[1], 1)))
+    t_model.add(LSTM(units=50, return_sequences=True, input_shape=(normed_train_data.shape[1], 3)))
 
     # Dropout layer is added to avoid over-fitting, which is a phenomenon where a machine learning model performs better on the training data compared to the test data
     t_model.add(Dropout(0.2))
@@ -141,61 +136,27 @@ def build_model():
 model = build_model()
 
 model.summary()
-
+# print(normed_train_data)
 early_stop = EarlyStopping(monitor='val_loss', patience=20)
 
 # TODO: REVISE THIS UGLY QUICK FIX
 train_labels = train_labels[:len(train_labels) - 10]
-history = model.fit(normed_train_data, train_labels, epochs=10,
+history = model.fit(normed_train_data, train_labels, epochs=500,
                     validation_split=0.2, verbose=1, callbacks=[early_stop])
 
 test_df = read_cleanse(train=False, dfevoo=dfevoo)
 test_df.pop('date')
 
-# test_df = pd.read_csv("food_dataset.csv")
-#
-# dfevoo = test_df[test_df['product'] == 'extra virgin olive oil (up to 0,8°)']
-# dfevoo = dfevoo[dfevoo['country'] == 'greece']
-# dfevoo['priceStringDate'] = pd.to_datetime(dfevoo['priceStringDate'])
-# dfevoo = dfevoo.drop(columns=['price_id', 'product', 'priceDate', 'url', 'country', 'dataSource']).sort_values(
-#     by='priceStringDate')
-# dfevoo = pd.DataFrame(dfevoo)
-# dfevoo = dfevoo.groupby('priceStringDate').mean().reset_index()
-# dfevoo['date'] = pd.to_datetime(dfevoo['priceStringDate'])
-# dfevoo['year'] = pd.DatetimeIndex(dfevoo['date']).year
-# dfevoo['month'] = pd.DatetimeIndex(dfevoo['date']).month
-# dfevoo['day'] = pd.DatetimeIndex(dfevoo['date']).day
-# columns = ['year', 'month', 'day', 'price']
-# dfall=dfevoo[columns]
-# test_date = '2018-01-01'
-# dfevoo = dfevoo[dfevoo['date'] >= test_date]
-#
-# columns = ['year', 'month', 'day', 'price']
-# test_df = dfevoo[columns]
-
-
 unknown_labels = test_df.pop(target)
-# print(len(unknown_labels))
-# print(unknown_labels)
-# unknown_stats = test_df.describe()
-#
-# print(unknown_stats)
-#
-# unknown_stats.pop(target)
-# unknown_stats = unknown_stats.transpose()
-# inputs = scaler.fit_transform(test_df)
-# inputs=dfall[len(dfall)-len(test_df)-lb:].values
+
 inputs = test_df
-print(inputs)
-print(len(inputs))
-print(inputs.shape[0])
+# print(inputs)
+# print(len(inputs))
+# print(inputs.shape[0])
 print(test_df.head())
 print(train_dataset.head())
 # inputs = inputs.reshape(-1,1)
-
-
-# print(inputs)
-
+# quit(0)
 
 inputs = scaler.transform(inputs)
 print(len(inputs))
@@ -204,12 +165,12 @@ X_test = []
 
 # for i in range(lb,len(inputs)):
 for i in range(lb, inputs.shape[0]):
-    X_test.append(inputs[i - lb:i, 0])
+    X_test.append(inputs[i - lb:i])
 # X_test = inputs
 # X_test.append(inputs[:,0])
 X_test = np.array(X_test)
 print(len(X_test))
-X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 3))
 
 unknown_predictions = model.predict(X_test)
 print(unknown_predictions)
