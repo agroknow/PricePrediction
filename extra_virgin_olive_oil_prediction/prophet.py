514d179a011f1ab2e3599457610be4cc4052b057
MihalisP <mihalispapak@gmail.com>
2019-07-18 09:49:09 +0300

    prophet v0.9

--- extra_virgin_olive_oil_prediction/prophet.py
+++ extra_virgin_olive_oil_prediction/prophet.py
@@ -2,6 +2,8 @@ import pandas as pd
 from fbprophet import Prophet
 import matplotlib.pyplot as plt
 
+# https://towardsdatascience.com/a-quick-start-of-time-series-forecasting-with-a-practical-example-using-fb-prophet-31c4447a2274
+
 test_date = '2019-01-01'
 columns = ['ds', 'y']
 df = pd.read_csv("food_dataset.csv")
@@ -11,6 +13,8 @@ df = df[df['country'] == 'greece']
 psd = pd.to_datetime(df['priceStringDate'])
 df['ds'] = psd
 df['y'] = df['price']
+
+df = df.groupby('ds').mean().reset_index()
 df = df[df['ds'] < test_date]
 df = df[columns]
 
@@ -22,7 +26,6 @@ m.fit(df)
 future = m.make_future_dataframe(periods=76)
 print(future.tail())
 
-
 df = pd.read_csv("food_dataset.csv")
 
 df = df[df['product'] == 'extra virgin olive oil (up to 0,8°)']
@@ -30,25 +33,30 @@ df = df[df['country'] == 'greece']
 psd = pd.to_datetime(df['priceStringDate'])
 df['ds'] = psd
 df['y'] = df['price']
+df = df.groupby('ds').mean().reset_index()
 df = df[df['ds'] >= test_date]
 df = df[columns]
 
 forecast = m.predict(future)
 forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
 
-print(forecast.info())
+forecast = forecast[forecast['ds'] >= test_date]
 
+print(forecast.info())
+print(forecast.head())
 plt.plot(forecast['yhat'].values)
+plt.plot(forecast['trend'].values)
 plt.plot(df['y'].values)
 # plt.plot(forecast['yhat_lower'].values)
 # plt.plot(forecast['yhat_upper'].values)
 plt.title('price prediction')
-plt.legend(['predicted', 'actual'], loc='upper left')
+plt.legend(['predicted', 'predicted2', 'actual'], loc='upper left')
 # plt.legend(['predicted', 'min', 'max'], loc='upper left')
 plt.show()
 
-fig2 = m.plot_components(forecast)
+fig1 = m.plot(forecast)
 plt.show()
-print(fig2)
 
-
+# fig2 = m.plot_components(forecast)
+# plt.show()
+# print(fig2)
