7955c3dfb9a19b35cc5e758380cf5e6612a30ad8
MihalisP <mihalispapak@gmail.com>
2019-07-24 11:45:43 +0300

    prophet v1.0

--- extra_virgin_olive_oil_prediction/prophet.py
+++ extra_virgin_olive_oil_prediction/prophet.py
@@ -1,62 +1,67 @@
+import random
+
 import pandas as pd
 from fbprophet import Prophet
 import matplotlib.pyplot as plt
 
 # https://towardsdatascience.com/a-quick-start-of-time-series-forecasting-with-a-practical-example-using-fb-prophet-31c4447a2274
 
-test_date = '2019-01-01'
+periods = 30
+
+# for periods in range(20, 65, 5):
+
 columns = ['ds', 'y']
 df = pd.read_csv("food_dataset.csv")
 
+# df = df[df['product'].str.contains('olive oil')]
 df = df[df['product'] == 'extra virgin olive oil (up to 0,8°)']
-df = df[df['country'] == 'greece']
-psd = pd.to_datetime(df['priceStringDate'])
-df['ds'] = psd
-df['y'] = df['price']
-
-df = df.groupby('ds').mean().reset_index()
-df = df[df['ds'] < test_date]
-df = df[columns]
-
-print(df.head())
-
-m = Prophet()
-m.fit(df)
-
-future = m.make_future_dataframe(periods=76)
-print(future.tail())
-
-df = pd.read_csv("food_dataset.csv")
-
-df = df[df['product'] == 'extra virgin olive oil (up to 0,8°)']
-df = df[df['country'] == 'greece']
+# df = df[df['country'] == 'greece']
 psd = pd.to_datetime(df['priceStringDate'])
 df['ds'] = psd
 df['y'] = df['price']
 df = df.groupby('ds').mean().reset_index()
-df = df[df['ds'] >= test_date]
+df = df.sort_values(by='ds')
 df = df[columns]
+df = df.set_index('ds').resample('d').ffill().reset_index()
 
-forecast = m.predict(future)
-forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
+random_vals = []
+for i in range(0, len(df['y'])):
+    random_vals.append(random.uniform(0.995, 1.005))
+df['y'] = df['y'] * random_vals
 
-forecast = forecast[forecast['ds'] >= test_date]
+train = df[:len(df) - periods]
+test = df[len(df) - periods:]
 
-print(forecast.info())
-print(forecast.head())
-plt.plot(forecast['yhat'].values)
-plt.plot(forecast['trend'].values)
-plt.plot(df['y'].values)
-# plt.plot(forecast['yhat_lower'].values)
-# plt.plot(forecast['yhat_upper'].values)
-plt.title('price prediction')
-plt.legend(['predicted', 'predicted2', 'actual'], loc='upper left')
-# plt.legend(['predicted', 'min', 'max'], loc='upper left')
-plt.show()
+for seasonality in range(7, 366, 5):
+    m = Prophet(weekly_seasonality=False, seasonality_mode='multiplicative')
+    m.add_seasonality(name='custom', period=seasonality, fourier_order=10)
+    # m = Prophet()
+    m.fit(train)
 
-fig1 = m.plot(forecast)
-plt.show()
+    future = m.make_future_dataframe(periods=periods)
 
-# fig2 = m.plot_components(forecast)
+    forecast = m.predict(future)
+    forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
+
+    plotted = forecast[len(forecast) - periods:]
+
+    # plt.plot(forecast['yhat'].values)
+    # plt.plot(forecast['trend'].values)
+    # plt.plot(df['y'].values)
+    # plt.title('price prediction' + str(periods))
+    # plt.legend(['predicted', 'trend', 'actual'], loc='upper left')
+    # plt.show()
+
+    plt.plot(plotted['yhat_lower'].values)
+    plt.plot(plotted['yhat'].values)
+    plt.plot(plotted['yhat_upper'].values)
+    # plt.plot(plotted['trend'].values)
+    plt.plot(test['y'].values)
+    plt.title('Olive Oil Price Prediction, seasonality: ' + str(seasonality))
+    # plt.legend(['predicted_lower', 'predicted_upper', 'predicted_mean', 'trend', 'actual'], loc='upper left')
+    plt.legend(['predicted_lower', 'predicted', 'predicted_upper', 'actual'], loc=(1.04,0))
+    plt.savefig('plots/s_' + str(seasonality) + 'prophet_' + str(periods) + '.png')
+    plt.show()
+
+# fig1 = m.plot(forecast)
 # plt.show()
-# print(fig2)
